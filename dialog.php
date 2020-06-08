333fc4e98c98cefe3b5ebed863473168236168e0
Liam Siira <liam@siira.us>
2020-06-08 18:34:05 -0400

    Initial Commit

---
+++ dialog.php
@@ -0,0 +1,7 @@
+<iframe width="100%" height="500px" id="dochub"></iframe>
+
+<button onclick="codiad.modal.unload();return false;">Close</button>
+
+<script>
+    $('#dochub').attr('src','http://www.dochub.io/#css/');
+</script>
